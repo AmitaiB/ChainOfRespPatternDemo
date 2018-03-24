bc26ccf2213a1b66f2f62805989e5e1b26418b4f
AmitaiB <blickstein@gmail.com>
2018-03-23 22:49:07 -0400

    Implements variation where all links in the chain are notified about a request (rarely used, but good for when links need to know when they've been pre-empted)

--- ChainOfResp/Transmitters.swift
+++ ChainOfResp/Transmitters.swift
@@ -8,19 +8,28 @@
 
 import Foundation
 
+class BoolWrapper: ExpressibleByBooleanLiteral {
+    var value: Bool
+    
+    required init(booleanLiteral value: BooleanLiteralType) {
+        self.value = value
+    }
+}
+
 /// Abstract base class for `Transmitter` objects.
 class Transmitter {
     var nextLink: Transmitter?
     
     required init() {}
     
-    func send(message: Message) -> Bool {
+    func send(message: Message, handled: BoolWrapper = false) -> Bool {
         if let next = nextLink {
-            return next.send(message: message)
-        } else {
+            return next.send(message: message, handled: handled)
+        } else if !handled.value {
             print("End of chain reached. Message not sent.")
             return false
         }
+        return handled.value
     }
     
     fileprivate class func matchEmailSuffix(with message: Message) -> Bool {
@@ -49,34 +58,37 @@ class Transmitter {
 
 
 class LocalTransmitter: Transmitter {
-    override func send(message: Message) -> Bool {
-        if Transmitter.matchEmailSuffix(with: message) {
+    override func send(message: Message, handled: BoolWrapper) -> Bool {
+        if !handled.value && Transmitter.matchEmailSuffix(with: message) {
             print("Message to: \(message.to) sent locally")
-            return true
-        } else {
-            return super.send(message: message)
+            handled.value = true
         }
+        return super.send(message: message, handled: handled)
     }
 }
 
 class RemoteTransmitter: Transmitter {
-    override func send(message: Message) -> Bool {
-        if !Transmitter.matchEmailSuffix(with: message) {
+    override func send(message: Message, handled: BoolWrapper) -> Bool {
+        if !handled.value && !Transmitter.matchEmailSuffix(with: message) {
             print("Message to: \(message.to) sent remotely")
-            return true
-        } else {
-            return super.send(message: message)
+            handled.value = true
         }
+        return super.send(message: message, handled: handled)
     }
 }
 
 class PriorityTransmitter: Transmitter {
-    override func send(message: Message) -> Bool {
-        if message.subject.lowercased().hasPrefix("priority") {
+    var totalMessages = 0
+    var handledMessages = 0
+    
+    override func send(message: Message, handled: BoolWrapper) -> Bool {
+        totalMessages += 1
+        if !handled.value && message.subject.lowercased().hasPrefix("priority") {
+            handledMessages += 1
             print("Message to: \(message.to) sent as priority")
-            return true
-        } else {
-            return super.send(message: message)
+            print("Stats: Handled \(handledMessages) of \(totalMessages)")
+            handled.value = true
         }
+        return super.send(message: message, handled: handled)
     }
 }
